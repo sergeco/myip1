39af3ab80d89cb179c05359ec896703cc32a6351
Sergey <wiamdj@mail.ru>
2026-02-11 15:47:09 +0700

    Initial commit for myip1

---
+++ src/myip1.sv
@@ -0,0 +1,16 @@
+module myip1 (
+    input  logic        clk_i,
+    input  logic        rst_ni,
+    input  logic        select_i,
+    output logic [15:0] data_o
+);
+
+    always_comb begin
+        if (select_i) begin
+            data_o = 16'hDEAD;
+        end else begin
+            data_o = 16'h0000;
+        end
+    end
+
+endmodule
