368d9ae40ee87d220679ad16e7023d4590fe2bb7
Sergey <wiamdj@mail.ru>
2026-02-25 10:23:59 +0700

    Change to 600

--- src/myip1.sv
+++ src/myip1.sv
@@ -1,16 +1,24 @@
 module myip1 (
-    input  logic        clk_i,
-    input  logic        rst_ni,
-    input  logic        select_i,
+    input  logic        clk,
+    input  logic        reset_n,
+    input  logic        data_i_valid,
+    input  logic [15:0] data_i,
+    output logic        data_o_valid,
     output logic [15:0] data_o
 );
 
-    always_comb begin
-        if (select_i) begin
-            data_o = 16'd501;
-        end else begin
-            data_o = 16'h0000;
-        end
+
+always_ff @(posedge clk or negedge reset_n) begin
+    if (!reset_n) begin
+        data_o       <= 16'b0;
+        data_o_valid <= 1'b0;
+    end else if (data_i_valid) begin
+        data_o       <= data_o + 16'd600;
+        data_o_valid <= 1'b1;
+    end else begin
+        data_o_valid <= 1'b0;
     end
+end
+
 
 endmodule
