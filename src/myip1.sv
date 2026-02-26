29a7218d5f7c5e6131701a7def143e26a645998c
Sergey <wiamdj@mail.ru>
2026-02-26 23:23:35 +0700

    Fix summation

--- src/myip1.sv
+++ src/myip1.sv
@@ -13,7 +13,7 @@ always_ff @(posedge clk or negedge reset_n) begin
         data_o       <= 16'b0;
         data_o_valid <= 1'b0;
     end else if (data_i_valid) begin
-        data_o       <= data_o + 16'd600;
+        data_o       <= data_i + 16'd600;
         data_o_valid <= 1'b1;
     end else begin
         data_o_valid <= 1'b0;
